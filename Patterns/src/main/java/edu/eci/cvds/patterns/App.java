199bb75351751d2890aef7f11546f023ce4fbb97
Juliana <erika.castro.romero@gmail.com>
2023-02-15 23:44:51 -0500

    actualizar

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/App.java
@@ -0,0 +1,24 @@
+package edu.eci.cvds.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name="";
+            for( String param : args) {
+                name+=param+" ";
+            }
+            if( "".contentEquals(name) ) {
+                System.out.println(   "Hello world!" );
+
+            }
+            else {
+                System.out.println(   "Hello "+name+"!" );
+            }
+        
+    }
+}
